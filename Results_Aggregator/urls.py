223574b03b69f31b634d7dbaf9c7406a0088e6fa
Vezral <Kenji1358@yahoo.com>
2018-05-20 16:54:07 +0800

    edited redirect

--- Results_Aggregator/urls.py
+++ Results_Aggregator/urls.py
@@ -25,8 +25,11 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('user/', include('user.urls')),
     path('competition/', include('competition.urls')),
-    re_path(r'^.*$', RedirectView.as_view(url='/'), name='homepage-redirect'),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += [
+    re_path(r'^.*$', RedirectView.as_view(url='/'), name='homepage-redirect'),
+]
